76252027aab8fc4b53d2904e267a0bd6490e5c9c
Anthony Bendeler <anthony.bendeler@capgemini.com>
2018-10-01 16:58:20 +0200

    finished post and get calls

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# json-server
-with vuejs
+# installing
+- install node@latest
+- create folder in which you want to clone the folder and change to folder
+- open command line:
+  1. copy paste the following: git clone https://github.com/Anthony1988/json-server.git
+  2. in installed folder => type in this command: npm install
+  3. type in the command: npm install json-server -g save
+
+# running
+- open a second command line in the same folder:
+  1. In the first command line window type in the command: json-server --watch ./src/fakeDB.json
+  2. In the second command line window type in the command: npm run dev
+  3. Open an internet browser and go to http://localhost:3000
--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,96 @@
 <template>
   <div id="app">
-    <div class="col-xs-12">
+    <div class="col-12">
+      <h1>Local Json Server with a json file as database</h1>
+      <h3>Made with VueJS</h3>
       <img src="./assets/logo.png">
     </div>
-    <div class="col-xs-12">
-      <button class="btn btn-primary" @click="getData">{{get}}</button>
-    </div>
-    <div class="col-xs-12">
-      <form>
+    <div class="col-12">
+      <form @submit.prevent="evaluateData">
         <label for="name">Your name</label>
-        <input name="name" type="text" placeholder="type your name" v-model="data.name" required />
+        <input name="name" type="text" placeholder="type your name" value="data.name" v-model="data.name" required />
+
         <label for="email">Your email</label>
-        <input name="email" type="email" placeholder="type your email" v-model="data.email" required />
-        <input type="submit" class="btn btn-secondary" @click.prevent="postData"> Send info </button>
+        <input name="email" type="email" placeholder="type your email" value="data.name" v-model="data.email" required />
+
+        <input type="submit" class="btn btn-secondary" v-model="submit">
+        <div id="already-submitted" v-if="alreadySubmitted">{{submitMsg}}</div>
       </form>
+    </div>
+    <div v-if="entries.length > 0" class="row" style="padding-top: 40px;">
+      <div class="col-4"></div>
+      <div class="col-4">
+        <table class="table" style="margin: auto">
+          <thead class="bg-warning">
+            <th scope="col">#</th>
+            <th scope="col">Name:</th>
+            <th scope="col">Email:</th>
+          </thead>
+          <tbody>
+            <tr v-for="(entry, index) in entries" v-bind:key="entry.email" class="bg-light">
+              <th scope="row">{{index}}</th>
+              <td>{{entry.name}}</td>
+              <td>
+                <span>{{entry.email}}</span>
+                <span style="float:right">
+                  <a href="" @click.prevent="deleteEntry(index)">&#10006; delete </a>
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="col-4"></div>
     </div>
   </div>
 </template>
 
 <script>
-const defaultData = {
-  name: undefined,
-  email: undefined,
-}
 export default {
   name: 'app',
   data () {
     return {
-      get: 'get All Data',
-      post: 'post Data',
-      data: defaultData,
+      submit: 'submit the data',
+      alreadySubmitted: false,
+      submitMsg: "The provided e-mail address is already used. Use a different e-mail address.", 
+      data: {},
+      entries: [],
     };
   },
   methods: {
     getData() {
       this.$axios
-        .get('https://jsonplaceholder.typicode.com/todos/', {
-          params: {
-            title: 'delectus aut autem',
-          }
-        })
-        .then(r => console.log(r.data[0]))
-        .then(data => console.log(data))
+        .get('/creds')
+        .then(r => this.setData(r.data))
+    },
+    setData(entries) {
+      this.entries = entries;
+    },
+    evaluateData() {
+      this.alreadySubmitted = this.entries.some(entry => entry.email === this.data.email);
+      if (!this.alreadySubmitted) {
+        this.postData();
+      }
     },
     postData() {
       const url = '/creds';
       this.$axios
         .post(url, this.data)
-        .then(() => this.data = defaultData)
+        .then(() => this.data = {})
+        .then(() => this.getData())
     },
-    sendIt() {
-      console.log(this.data);
-    }
+    deleteEntry(index) {
+      const data = this.entries[index];
+      const id = data.id;
+      const url = `/creds/${id}`;
+      this.$axios
+        .delete(url, data)
+        .then(() => this.getData());
+    },
   },
+  created() {
+    this.getData()
+  }
 }
 </script>
 
@@ -82,5 +120,11 @@ li {
 
 a {
   color: #42b983;
+}
+
+#already-submitted {
+  padding-top: 10px;
+  color: red;
+  font-weight: bold;
 }
 </style>
